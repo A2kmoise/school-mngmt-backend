d98de5f0e581889894e12f171638d6b8a30567ab
Moise <moise@Moise.Moise>
2025-04-24 19:46:11 +0200

    mis 1.0.0

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import router from './routes/students_routes.js'
 import dotenv from 'dotenv'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger/swagger'
 import Mongoconn from './config/dbConnection.js'
 
 
@@ -16,10 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('images'));
 app.use(router);
 
-
-    
-
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);
